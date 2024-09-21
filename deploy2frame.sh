2f9339a74d53c672727921ab5cdb0817dbcf96a6
Tomas.Beran <Tomas.Beran@gmail.com>
2024-09-21 23:03:46 +0200

    deleted:    deploy2frame.sh

--- deploy2frame.sh
+++
@@ -1,1 +0,0 @@
-rsync -av -e ssh --exclude='*.venv' --exclude='*.pyc' --exclude "*.jpg" --exclude "__pycache__" --exclude ".git" --exclude ".vscode" --exclude "*.log" --exclude "config.py" /home/tom/data/git/samsung-frame/samsung-photo-frame-ctrl/samsung-photo-frame-ctrl pi@192.168.1.106:/home/pi/frame
