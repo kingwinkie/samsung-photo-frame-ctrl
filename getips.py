db28cedcb21f7d98080c6861a778aaf24f510b8c
bero158 <tomas.beran@gmail.com>
2024-11-27 20:43:36 +0100

    modified:   getips.py

--- getips.py
+++ getips.py
@@ -8,22 +8,23 @@ except ImportError:
     netifaces = None
     pass  # When running the setup.py, netifaces is not yet installed
 
+
 LOCAL_IPS : list [str] = ["lo","127.0.0.1"]
 
 def getIPList():
     addrList = []
-    ifaces : list = netifaces.interfaces()
-    if ifaces:
-        
-        for iface in ifaces:
-            ifcAddresses = netifaces.ifaddresses(iface)
-            if ifcAddresses:
-                ifcInetAddresses = ifcAddresses.get(netifaces.AF_INET)
-                if ifcInetAddresses:
-                    for inetAddr in ifcInetAddresses:
-                        addr = inetAddr.get("addr")
-                        if addr and addr not in LOCAL_IPS:
-                            addrList.append(addr)
+    if netifaces:
+        ifaces : list = netifaces.interfaces()
+        if ifaces:
+            for iface in ifaces:
+                ifcAddresses = netifaces.ifaddresses(iface)
+                if ifcAddresses:
+                    ifcInetAddresses = ifcAddresses.get(netifaces.AF_INET)
+                    if ifcInetAddresses:
+                        for inetAddr in ifcInetAddresses:
+                            addr = inetAddr.get("addr")
+                            if addr and addr not in LOCAL_IPS:
+                                addrList.append(addr)
     return addrList
 
 def getIPStr(addrList : list = None, sep : str = ", "):
